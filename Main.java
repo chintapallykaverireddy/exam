cc6d6f3a106221cb19fefed4f201acbae504d0b4
kaverireddy <kaverireddy267@gmail.com>
2024-12-11 14:45:48 +0530

    intial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+ public static void main(String args[]) {
+   System.out.println("Hello from java");
+  int a=5;
+  int b=10;
+ int res=a+b;
+System.out.println("The sum is  " +res);
+}
+}
---
+++ file.py
@@ -0,0 +1,4 @@
+print("Hello from python")
+a=5
+b=3
+print("The multiplication is ",(a*b))
